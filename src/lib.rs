9733879322dc1779f7d5f3885680685f998341c0
csking101 <chinmayasahu101@gmail.com>
2024-02-06 11:10:45 +0530

    Removed main from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,2 @@
 pub mod hashing;
 
-fn main() {
-    let long_url = "https://www.google.com/search?channel=fs&client=ubuntu-sn&q=meow+meow".to_string();
-    let rate_limit = Some(500000000000);
-    let permission_rule = None;
-
-    let request = hashing::URLCreationDescription {
-        long_url,
-        rate_limit,
-        permission_rule,
-    };
-
-    println!("The request struct is {:?}", request);
-    println!("The hash is {:?}", request.get_hash());
-}
