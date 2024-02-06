cf9956a790908688adb63d5b87a2390ededd1fb4
csking101 <chinmayasahu101@gmail.com>
2024-02-06 11:10:06 +0530

    Converted binary into library and added example

---
+++ examples/simple_hashing.rs
@@ -0,0 +1,16 @@
+extern crate url_shortener;
+
+fn main() {
+    let long_url = "https://www.google.com/search?channel=fs&client=ubuntu-sn&q=meow+meow".to_string();
+    let rate_limit = Some(500000000000);
+    let permission_rule = None;
+
+    let request = url_shortener::hashing::URLCreationDescription {
+        long_url,
+        rate_limit,
+        permission_rule,
+    };
+
+    println!("The request struct is {:?}", request);
+    println!("The hash is {:?}", request.get_hash());
+}
